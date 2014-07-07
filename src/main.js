0d4382eeb82095efcdd35369708689029e8855be
Imti <imtiazmaj@gmail.com>
2014-07-07 16:02:02 -0700

    Add smooth rotation and translation for 1 object

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ define(function(require, exports, module) {
     var RenderNode = require('famous/core/RenderNode');
     var KeyCodes   = require('famous/utilities/KeyCodes');
     var ScrollSync = require('famous/inputs/ScrollSync');
+    var Quaternion = require('famous/math/Quaternion');
 
     // Main Context
     var mainContext = Engine.createContext();
@@ -26,6 +27,23 @@ define(function(require, exports, module) {
     });
 
     // Individuals
+    var squareScroll = new ScrollSync();
+    var quaternion = new Quaternion(100, 0, 0, 0);
+    var smallQuaternion = new Quaternion(100, 0, 0, 0);
+
+    var rotationModifier = new Modifier({
+      origin: [0.5, 0.5],
+      align: [0.5, 0.5]
+    });
+
+    rotationModifier.setTransform(function() {
+      return quaternion.getTransform();
+    });
+
+    Engine.on('prerender', function() {
+      quaternion = quaternion.multiply(smallQuaternion);
+    });
+
     var square = new Surface({
       size: [300, 300],
       classes: ['double-sided'],
@@ -34,19 +52,58 @@ define(function(require, exports, module) {
         fontSize: '40px',
         lineHeight: '300px',
         textAlign: 'center'
-      }
+      },
+      transform: Transform.translate(0, 0, 150)
     });
+    Engine.pipe(squareScroll);
+
+    Engine.on('keyup', function() {
+      console.log('keyup');
+      Engine.unpipe(mainScroll);
+      Engine.pipe(squareScroll);
+    });
+
+    squareScroll.on('update', function(data) {
+      if(data.delta[0] < 0) {
+        smallQuaternion.y += data.delta[0];
+        if(smallQuaternion.y > 0) smallQuaternion.y = -50;
+      }
+      if(data.delta[0] > 0) {
+        smallQuaternion.y += data.delta[0];
+        if(smallQuaternion.y < 0) smallQuaternion.y = 50;
+      }
+      if(data.delta[1] < 0) {
+        smallQuaternion.x += data.delta[1];
+        if(smallQuaternion.x > 0) smallQuaternion.x = -50;
+      }
+      if(data.delta[1] > 0) {
+        smallQuaternion.x += data.delta[1];
+        if(smallQuaternion.x < 0) smallQuaternion.x = 50;
+      }
+
+      smallQuaternion.x = smallQuaternion.x/10;
+      smallQuaternion.y = smallQuaternion.y/10;
+    });
+
+    squareScroll.on('end', function(data) {
+      smallQuaternion.x = 0;
+      smallQuaternion.y = 0;
+    });
+
     nodeOfSquares.add(square);
+
 
     // Main controls -- translation
     Engine.on('keypress', function(keyPressed) {
+      // console.log('keypress')
       // W = 119, A = 97, S = 115, D = 100
       // console.log(keyPressed.charCode);
       if(keyPressed.charCode === 96) {
         // console.log("TILDA");
+        Engine.unpipe(squareScroll);
         Engine.pipe(mainScroll);
         mainScroll.on('update', function(data) {
-            console.log(data.delta)
+            // console.log(data.delta)
             if(data.delta[1] > 0) {
                 mainZ+=data.delta[1]/10
                 translationModifier.setTransform(function() {
@@ -77,7 +134,6 @@ define(function(require, exports, module) {
                     return Transform.translate(mainX, mainY, mainZ);
                 });
             }
-            Engine.unpipe(mainScroll);
         });
       }
     });
@@ -85,6 +141,6 @@ define(function(require, exports, module) {
 
 
 
-    mainContext.add(translationModifier).add(nodeOfSquares);
+    mainContext.add(translationModifier).add(rotationModifier).add(nodeOfSquares);
 
 });
